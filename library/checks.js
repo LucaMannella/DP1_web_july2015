aae18d856efc32b637e72a135b395bfe78d62178
Luke <luca.mannella@outlook.it>
2015-07-07 01:54:39 +0200

    Rimossi warning dal file checks.js

--- library/checks.js
+++ library/checks.js
@@ -1,9 +1,9 @@
 /** This file is responsible for managing client-side controls. **/
 
 function checkRegistrationValues(){
-	user = document.getElementById("Username").value;
-	pwd = document.getElementById("Password").value;
-	conf_pwd = document.getElementById("ConfirmPassword");
+    var user = document.getElementById("Username").value;
+	var pwd = document.getElementById("Password").value;
+	var conf_pwd = document.getElementById("ConfirmPassword");
 	if( (user==="")||(pwd==="")||(conf_pwd==="") ) {
 		window.alert("You miss a field! Please fill it before send your request!");
 		return false;
@@ -18,8 +18,8 @@ function checkRegistrationValues(){
 	}
 }
 function checkLoginValues(){
-	user = document.getElementById("Username").value;
-	pwd = document.getElementById("Password").value;
+    var user = document.getElementById("Username").value;
+    var pwd = document.getElementById("Password").value;
 	if( (user==="")||(pwd==="") ) {
 		window.alert("You miss a field! Please fill it before send your request!");
 		return false;
@@ -29,10 +29,11 @@ function checkLoginValues(){
 }
 
 function checkPassword() {
-	pwd1 = document.getElementById("Password").value;
-	pwd2 = document.getElementById("ConfirmPassword").value;
-	img = document.getElementById("imgpwd");
+    var pwd1 = document.getElementById("Password").value;
+	var pwd2 = document.getElementById("ConfirmPassword").value;
+	var img = document.getElementById("imgpwd");
 
+    var toRet;
 	if(pwd1===pwd2) {
 		img.setAttribute("src", "./images/right.png");
 		toRet=true;
@@ -43,20 +44,6 @@ function checkPassword() {
 	}
 	img.style.visibility = "visible";
 	return toRet;
-}
-
-function checkAvailability(code){
-	var form = document.getElementById("activity"+code);
-	var childs = parseInt( form[1].value );
-	var availability = parseInt( document.getElementById("av"+code).innerHTML );
-
-	//DEBUG console.log(availability);	console.log(form[1].value);
-	if((childs+1) <= availability)
-		return true;
-	else {
-		window.alert("There are not so many places to reserve! Please reduce the number of children!");
-		return false;
-	}
 }
 
 function checkConferenceValues() {
